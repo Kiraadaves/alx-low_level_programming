2259b9ed4f25c2e0132ad715dee00c1085fdeedd
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-07-01 16:03:05 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point to print single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 0;
+
+	for (ch = 0; ch > 10; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n')
+	return (0);
+}
