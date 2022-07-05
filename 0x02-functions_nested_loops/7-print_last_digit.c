7c099c925b76b0757ae1dc8dbe46241d713e943c
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-07-05 09:14:03 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - will print the last digit of a number
+ *
+ * @n: variable that stores the character
+ *
+ * Return: 0 for success
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
