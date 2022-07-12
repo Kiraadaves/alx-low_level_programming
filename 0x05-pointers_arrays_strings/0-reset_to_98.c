cb4a9bd05ddc103089fd03dc65ffe29ab73c07a3
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-07-12 08:53:43 -0700

    update to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
-* reset_to_98 -a function that takes a pointer to an int
-* @n: pointer
-* Return: (0)
-*/
+ * reset_to_98 - changes pinter value to 98
+ * @n: integer variable for function
+ *
+ * Return: 0
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
