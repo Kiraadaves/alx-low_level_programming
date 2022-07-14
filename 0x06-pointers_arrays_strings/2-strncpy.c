2c05320a5b886389d5aea9f77728c0d7b94b05a5
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-07-14 21:42:17 +0100

    Update 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strncpy - copies the string includimg the terminating null byte
+ *@dest: buffer storing the string copy
+ *@src: the source string
+ *@n: max number of byte copied
+ *Return: returns
+ */
+ 
+ char *_strncpy(char *dest, char *src, int n)
+ {
+ 			
