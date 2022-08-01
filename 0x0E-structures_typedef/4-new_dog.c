57637d85af4701d2844c347d964e2e2d1ff84548
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-08-01 10:56:36 -0700

    4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,52 +2,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-*new_dog - struct returning
-*@name: dog name
-*@age: age of dog
-*@owner: owner name
-*Return: dog struct
-*/
+ * new_dog - struct returning
+ * @name: member of struct
+ * @age: member
+ * @owner: member
+ * Return: dog struct
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog1;
-	int nameSize = 0, ownerSize = 0, loop;
+	int nameSize = 0, ownersize = 0, loop;
 
-while (name[nameSize] != '\0')
-{
-nameSize++;
-}
-while (owner[ownerSize] != '\0')
-{
-ownerSize++;
-}
-dog1 = malloc(sizeof(dog_t));
-if (dog1 == NULL)
-{
-free(dog1);
-return (NULL);
-}
-dog1->name = malloc(sizeof(dog1->name) * nameSize);
-if (dog1->name == NULL)
-{
-free(dog1->name);
-free(dog1);
-return (NULL);
-}
-for (loop = 0; loop <= nameSize; loop++)
-{
-dog1->name[loop] = name[loop];
-}
-dog1->age = age;
-dog1->owner = malloc(sizeof(dog1->owner) * ownerSize);
-if (dog1->owner == NULL)
-{
-free(dog1->owner);
-free(dog1->name);
-free(dog1);
-return (NULL);
-}
-for (loop = 0; loop <= ownerSize; loop++)
-dog1->owner[loop] = owner[loop];
-return (dog1);
+	while (name[nameSize] != '\0')
+	{
+		nameSize++;
+	}
+	while (owner[ownersize] != '\0')
+	{
+		ownersize++;
+	}
+	dog1 = malloc(sizeof(dog_t));
+	if (dog1 == NULL)
+	{
+		free(dog1);
+		return (NULL);
+	}
+	dog1->name = malloc(sizeof(dog1->name) * nameSize);
+	if (dog1->name == NULL)
+	{
+		free(dog1->name);
+		free(dog1);
+		return (NULL);
+	}
+	for  (loop = 0; loop <= nameSize; loop++)
+	{
+		dog1->name[loop] = name[loop];
+	}
+	dog1->age = age;
+	dog1->owner = malloc(sizeof(dog1->owner) * ownersize);
+	if (dog1->owner == NULL)
+	{
+		free(dog1->owner);
+		free(dog1->name);
+		free(dog1);
+		return (NULL);
+	}
+	for (loop = 0; loop <= ownersize; loop++)
+		dog1->owner[loop] = owner[loop];
+	return (dog1);
 }
