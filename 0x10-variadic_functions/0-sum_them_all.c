d7383f2ad203bb0ad33627089ff7f3b82a1887c6
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-08-04 15:00:30 -0700

    correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	for (i = 0; i < n; i++)
 	{
-		int x = va_arg(args, int);
+		int x = va_arg(args, unsigned int);
 
 		count = count + x;
 	}
