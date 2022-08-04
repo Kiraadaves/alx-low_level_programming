892c59746df7036b4b5a15f6ae1a5f472cff3109
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-08-04 16:52:38 -0700

    correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,16 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	if (separator == NULL)
-	{
-		;
-	}
 	for (i = 0; i < n; i++)
 	{
 	int x = va_arg(args, int);
 
 	printf("%d", x);
-	if (i != (n - 1))
+	if (i != (n - 1) && separator != NULL)
 	printf("%s", separator);
 	}
 	printf("\n");
