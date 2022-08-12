c7f4539214dc4f30e09e2edfd2cdd5da711c0327
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-08-11 17:25:37 -0700

    correction of task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,10 +10,10 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head != NULL)
+	while ((temp = head) != NULL)
 	{
-		temp = head;
 		head = head->next;
+		free(temp->str);
 		free(temp);
 	}
 }
