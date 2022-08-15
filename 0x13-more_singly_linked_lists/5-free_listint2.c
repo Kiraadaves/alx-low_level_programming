61ad48aafa32109cccc48c7802aa6e3d55e24b61
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-08-15 13:36:58 -0700

    free list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *newhead, *temp;
 
-	if (*head == NULL)
-		return;
-
-	while (*head != NULL)
+	if (*head != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
+		newhead = *head;
+	while (newhead != NULL)
+	{
+		temp = newhead;
+		newhead = newhead->next;
 		free(temp);
+	}
 	}
 	*head = NULL;
 }
