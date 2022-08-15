8d37874700700c34f818531976664f34af7c71af
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-08-15 13:50:13 -0700

    list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,12 +13,12 @@ void free_listint2(listint_t **head)
 	if (*head != NULL)
 	{
 		newhead = *head;
-	while (newhead != NULL)
-	{
-		temp = newhead;
-		newhead = newhead->next;
-		free(temp);
-	}
-	}
+		while (newhead != NULL)
+		{
+			temp = newhead;
+			newhead = newhead->next;
+			free(temp);
+		}
 	*head = NULL;
+	}
 }
