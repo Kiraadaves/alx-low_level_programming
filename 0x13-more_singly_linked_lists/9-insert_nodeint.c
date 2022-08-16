5244ef716f9664f7a6f8b88ea261903fb8090304
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-08-16 14:29:44 -0700

    correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- * listlen - returns the length of a list
- * @h: pointer to head node
- * Return: length of list
- */
-size_t listlen(listint_t *h)
-{
-	unsigned int count = 0;
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-/**
  * insert_nodeint_at_index - inserts a node at nth position
  * @head: pointer to pointer to head node
  * @idx: position to insert
@@ -25,23 +8,30 @@ size_t listlen(listint_t *h)
  *
  * Return: pointer to list with added new node
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int x = listlen(*head);
-	unsigned int i = 1;
+	unsigned int x;
 	listint_t *newnode, *temp;
 
 	newnode = malloc(sizeof(listint_t));
-	if ((newnode == NULL) || (idx > x))
+	if (newnode == NULL)
 		return (NULL);
-	temp = *head;
-	while (i < idx)
-	{
-		temp = temp->next;
-		i++;
-	}
 	newnode->n = n;
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	temp = *head;
+	for (x = 0; x < (idx - 1); x++)
+	{
+		if (temp == NULL || temp->next == NULL)
+			return (NULL);
+		temp = temp->next;
+	}
 	newnode->next = temp->next;
 	temp->next = newnode;
-	return (*head);
+	return (newnode);
 }
