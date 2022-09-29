5868f574bb7af20654bb20acbf5858c6b4b0220b
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-09-29 16:00:47 -0700

    task 8

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,6 +10,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode, *temp;
+
 	newnode = malloc(sizeof(dlistint_t));
 
 	if (newnode == NULL)
@@ -28,6 +29,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	while (temp->next != NULL)
 		temp = temp->next;
 	temp->next = newnode;
-	newnode->prev =temp;
+	newnode->prev = temp;
 	return (*head);
 }
